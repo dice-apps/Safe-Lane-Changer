b5de13d736449e43663a3f3846a5cce07b80166a
dice-apps <disanda63@gmail.com>
2025-10-28 11:20:11 +0530

    Motor controller logic added

--- Src/main.c
+++ Src/main.c
@@ -1,8 +1,8 @@
 //Working code, modification of HAL code
 //Operation Successful
 //MCU Clock speed set at 8MHz
-//PSC = 200 - 1
-
+//PSC TIM2 = 200 - 1
+//DC Motor Control Code and Servo
 
 #include <stdint.h>
 
@@ -18,6 +18,7 @@
 // GPIOA base
 #define GPIOA_BASE      0x40010800UL
 #define GPIOA_CRL       (*(volatile unsigned int*)(GPIOA_BASE + 0x00))
+#define GPIOA_ODR		(*(volatile unsigned int*)(GPIOA_BASE + 0x0C))
 
 // TIM2 base
 #define TIM2_BASE       0x40000000UL
@@ -29,44 +30,61 @@
 #define TIM2_CCR1       (*(volatile unsigned int*)(TIM2_BASE + 0x34))
 #define TIM2_EGR        (*(volatile unsigned int*)(TIM2_BASE + 0x14))
 
+// TIM3 base
+#define TIM3_BASE		0x40000400
+#define TIM3_CR1        (*(volatile unsigned int*)(TIM3_BASE + 0x00))
+#define TIM3_CCMR1      (*(volatile unsigned int*)(TIM3_BASE + 0x18))
+#define TIM3_CCER       (*(volatile unsigned int*)(TIM3_BASE + 0x20))
+#define TIM3_PSC        (*(volatile unsigned int*)(TIM3_BASE + 0x28))
+#define TIM3_ARR        (*(volatile unsigned int*)(TIM3_BASE + 0x2C))
+#define TIM3_CCR1       (*(volatile unsigned int*)(TIM3_BASE + 0x34))
+#define TIM3_EGR        (*(volatile unsigned int*)(TIM3_BASE + 0x14))
+
+
 // SysTick base
 #define SYSTICK_BASE    0xE000E010UL
 #define SYSTICK_CTRL    (*(volatile unsigned int*)(SYSTICK_BASE + 0x00))
 #define SYSTICK_LOAD    (*(volatile unsigned int*)(SYSTICK_BASE + 0x04))
 #define SYSTICK_VAL     (*(volatile unsigned int*)(SYSTICK_BASE + 0x08))
 
+// Alternate Functions
+#define AFIO_BASE		0x40010000UL
+#define AFIO_MAPR		(*(volatile unsigned int*)(AFIO_BASE + 0x04))
+
 // ========================
 // Bit definitions
 // ========================
 
 // RCC
-#define RCC_IOPAEN      (1 << 2)   // GPIOA clock enable
+/*#define RCC_IOPAEN      (1 << 2)   // GPIOA clock enable
 #define RCC_TIM2EN      (1 << 0)   // TIM2 clock enable
+*/
 
 // TIM2
-#define TIM_CR1_CEN     (1 << 0)
+/*#define TIM_CR1_CEN     (1 << 0)
 #define TIM_CR1_ARPE    (1 << 7)
 #define TIM_EGR_UG      (1 << 0)
 #define TIM_CCMR1_OC1M_PWM1 (6 << 4)
 #define TIM_CCMR1_OC1PE (1 << 3)
 #define TIM_CCER_CC1E   (1 << 0)
+*/
 
 // SysTick
-#define SYSTICK_CLKSRC  (1 << 2)
+/*#define SYSTICK_CLKSRC  (1 << 2)
 #define SYSTICK_ENABLE  (1 << 0)
 #define SYSTICK_COUNTFLAG (1 << 16)
-
+*/
 // ========================
 // Global variables
 // ========================
-volatile uint32_t pwm_val = 0;
-volatile unsigned char direction = 0;
+//volatile uint32_t pwm_val = 0;
+//volatile unsigned char direction = 0;
 
 #define RCC_CR      (*(volatile unsigned int*)(RCC_BASE + 0x00))
 #define RCC_CFGR    (*(volatile unsigned int*)(RCC_BASE + 0x04))
 #define FLASH_ACR   (*(volatile unsigned int*)0x40022000)
 
-void clock_init(void) {
+/*void clock_init(void) {
     // Enable HSE
     RCC_CR |= (1 << 16);
     while (!(RCC_CR & (1 << 17))); // wait for HSE ready
@@ -86,13 +104,13 @@ void clock_init(void) {
     RCC_CFGR &= ~(3 << 0);
     RCC_CFGR |= (2 << 0);
     while (((RCC_CFGR >> 2) & 0x3) != 0x2); // wait for switch
-}
+}*/
 
 
 // ========================
 // Delay using SysTick
 // ========================
-void delay_ms(unsigned int ms)
+/*void delay_ms(unsigned int ms)
 {
     SYSTICK_LOAD = 72000 - 1;     // 72MHz/1000 = 72k ticks/ms
     SYSTICK_VAL = 0;
@@ -103,7 +121,7 @@ void delay_ms(unsigned int ms)
     }
     SYSTICK_CTRL = 0;
 }
-
+*/
 void servo_angle(uint8_t angle)
 {
 	uint16_t pulse = 50 + ((50*angle)/120);
@@ -116,33 +134,132 @@ void delay(uint32_t time)
 	while(time--);
 }
 
+void pin_enable(void)
+{
+	//PA0-> TIMER2
+	//PA6-> TIMER3
+
+	// 1. Enable clocks
+    RCC_APB2ENR |= (1 << 2);   // GPIOA clock enable
+    RCC_APB1ENR |= (1 << 0);   // TIM2 clock enable for Servo PWM -> 50Hz
+    RCC_APB1ENR |= (1 << 1); //TIM3 clock enable for DC motor PWM -> 100kHz
+    RCC_APB2ENR |= (1 << 0); //AFIO clock enable, to define AF
+
+
+    // PA0 = AF Push-Pull (MODE=11, CNF=10), TIM2_CH1
+	// PA6 = AF Push-Pull (MODE=11, CNF=10), TIM3_CH1
+	// PA2 = GPO Push Pull (MODE=01, CNF=00)
+	// PA3 = GPO Push Pull (MODE=01, CNF=00)
+
+    GPIOA_CRL &= ~(0xFFFF << 0); //Clear bits
+    GPIOA_CRL |=  (0xB << 0); //Set PA0
+    GPIOA_CRL &= ~(0xF << 24); //Set PA6 bits initially to zero
+    GPIOA_CRL |= (0xB << 24); //Set PA6
+    GPIOA_CRL |= (0x1 << 8); //Set PA2
+    GPIOA_CRL |= (0x1 << 12); //Set PA3
+
+    //Setting no re-map for alternate function pins PA0 and PA6
+    AFIO_MAPR &= ~(0x3 << 8); //Set TIM1 no re-map (00)
+    AFIO_MAPR &= ~(0x3 << 10); //Set TIM3 not re-map (00)
+}
+
+void TIM2_init(void)
+{
+    //Configure TIM2
+    TIM2_PSC = 200-1; //Prescaler
+    TIM2_ARR = 800-1; //Period
+    TIM2_CCR1 = 75; // Duty cycle start/ Reference position could be provided
+
+    TIM2_CCMR1 &= ~(0x7 << 4);
+    TIM2_CCMR1 |= (0x6 << 4) | (1 << 3); //(6 << 4) | (1 << 3)
+    TIM2_CCER |= (1 << 0); //Set output of pwm as output of corresponding pin
+
+    TIM2_CR1 |= (1 << 7); //Auto reload/ pre-load enable
+    TIM2_EGR |= (1 << 0); //Set UG
+    TIM2_CR1 |= (1 << 0); //Counter enabled
+}
+
+void TIM3_init(void)
+{
+    //Configure TIM2
+    TIM3_PSC = 7; //Pre-scaler
+    TIM3_ARR = 1000-1; //Period, TIM3 considered as 8MHz as operated in default SYSCLK of 8MHz
+    TIM3_CCR1 = 500; // Duty cycle start (50% duty)/ Reference speed could be provided
+
+    TIM3_CCMR1 &= ~(0x7 << 4);
+    TIM3_CCMR1 |= (0x6 << 4) | (1 << 3);
+    TIM3_CCER |= (1 << 0); //Set output of pwm as output of corresponding pin
+
+    TIM3_CR1 |= (1 << 7); //Auto reload/ pre-load enable
+    TIM3_EGR |= (1 << 0); //Set UG
+    TIM3_CR1 |= (1 << 0); //Counter enabled
+}
+
+
+
+void forward_dir(void)
+{
+	GPIOA_ODR |= (1 << 2); //PA2 high
+	GPIOA_ODR &= ~(1 << 3); //PA3 low
+}
+
+void backward_dir(void)
+{
+	GPIOA_ODR &= ~(1 << 2); //PA2 low
+	GPIOA_ODR |= (1 << 3); //PA3 high
+}
+
+void stop(void)
+{
+	GPIOA_ODR &= ~((1 << 2) | (1 << 3)); //PA2 and PA3 low
+}
+
+void speed(int speed) //Input 'speed' should be in a range of 0 - 100. Note: Error handling done
+{
+	int duty;
+
+	if ((speed > 0) | (speed < 90)) //Safe limits are set to the motor, assuming speed 100 is the maximum
+	{
+		duty = (10 * speed); //This happens with ((1000/100) * speed) where thousand is the total period TIM_ARR and 100 is the range of the speed variable.
+		TIM3_CCR1 = duty;
+	}
+	else if(speed == 0)
+	{
+		stop();
+	}
+}
+
+void set_dir_dcmotor(uint8_t dir)
+{
+	//dir = 1-> Forward direction
+	//dir = 0-> Backward direction
+	//dir = any-> Stop
+	if(dir == 1)
+	{
+		forward_dir();
+	}
+	else if(dir == 0)
+	{
+		backward_dir();
+	}
+	else stop();
+}
+
 // ========================
 // Main Program
 // ========================
 int main(void)
 {
     //clock_init();
+	pin_enable();
+	TIM2_init();
+	TIM3_init();
 
-	// 1. Enable clocks
-    RCC_APB2ENR |= RCC_IOPAEN;   // GPIOA clock enable
-    RCC_APB1ENR |= RCC_TIM2EN;   // TIM2 clock enable
 
-    // 2. Configure PA0 = Alternate Function Push-Pull (MODE=11, CNF=10)
-    GPIOA_CRL &= ~(0xF << (0 * 4));
-    GPIOA_CRL |=  (0xB << (0 * 4));
 
-    // 3. Configure TIM2
-    TIM2_PSC = 200-1;        // Prescaler
-    TIM2_ARR = 800-1;     // Period
-    TIM2_CCR1 = 75;        // Duty cycle start/ Reference position could be provided
 
-    TIM2_CCMR1 &= ~(7 << 4);
-    TIM2_CCMR1 |= TIM_CCMR1_OC1M_PWM1 | TIM_CCMR1_OC1PE;
-    TIM2_CCER |= TIM_CCER_CC1E;
 
-    TIM2_CR1 |= TIM_CR1_ARPE;
-    TIM2_EGR |= TIM_EGR_UG;
-    TIM2_CR1 |= TIM_CR1_CEN;
+
 
     // 4. Main loop: PWM sweep
     while (1) {
@@ -160,7 +277,7 @@ int main(void)
 
     	delay(9000000);
 
-        //TIM2_CCR1 =100;
+        /*//TIM2_CCR1 =100;
     	servo_angle(0);
         //delay_ms(1);
         delay(9000000);
@@ -173,7 +290,29 @@ int main(void)
         //TIM2_CCR1 = 60;
         //delay_ms(1);
         servo_angle(50);
-        //delay(9000000);
+        //delay(9000000);*/
+
+    	set_dir_dcmotor(1);
+    	//servo_angle(100);
+
+    	delay(9000000);
+
+    	set_dir_dcmotor(0);
+
+    	delay(9000000);
+
+    	set_dir_dcmotor(2);
+
+    	delay(9000000);
+
+    	speed(40);
+    	set_dir_dcmotor(1);
+    	delay(9000000);
+    	speed(85);
+    	delay(9000000);
+    	speed(40);
+    	delay(9000000);
+
 
     }
 
